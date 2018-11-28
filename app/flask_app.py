43da906945357f93b41f9cbcf00a5e9052b241b8
Vikas Phatak <vikibytes@users.noreply.github.com>
2018-11-28 09:23:36 +0530

    main flask file
    1. added default routes

---
+++ app/flask_app.py
@@ -0,0 +1,52 @@
+from app import app
+import pandas as pd
+import numpy as np
+
+import random
+from io import BytesIO
+# from StringIO import StringIO  # python 2.7x
+import matplotlib.pyplot as plt
+from flask import make_response, render_template
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import base64
+
+plt.style.use('seaborn')
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    df = pd.read_csv('/home/vikibytes/mysite/app/dbtable.csv')
+    htm = df.to_html()
+    return htm
+    
+@app.route('/vikas')
+def my_barplot():
+    """
+    reads a csv file where the first column is a count and the second is a date
+    and plots the last 30 entries
+    """
+    df = pd.read_csv('/home/vikibytes/mysite/app/dbtable.csv')
+    #df = pd.read_excel("data/data.xlsx", header=None, names=['count', 'date'])
+
+    x_labels = df['CallDate'].values
+    x_coordinates = np.arange(len(x_labels))
+
+    bar_heights = df['count'].values
+    plt.bar(x_coordinates, bar_heights, align='center', color=['lightblue'], edgecolor=['grey'])
+    plt.xticks(x_coordinates, x_labels, rotation='vertical')
+
+    plt.ylabel('Count')
+    plt.title('simple bar chart')
+    plt.tight_layout()
+    plt.savefig('pic.png')
+
+    ### Rendering Plot in Html
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue())
+    result = str(figdata_png)[2:-1]
+    return render_template('output.html', result=result)
